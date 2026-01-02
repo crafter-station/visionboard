f09dd72df69088cdc7725078999c36c135c9ff7f
Cris <cristian.correa.cs@gmail.com>
2026-01-01 23:03:49 -0500

    update fal config

--- lib/fal.ts
+++ lib/fal.ts
@@ -5,7 +5,7 @@ fal.config({
 });
 
 export async function pixelateImage(imageUrl: string): Promise<string> {
-  //const stream = await fal.stream("fal-ai/gpt-image-1.5/edit", {
+  
     const result = await fal.subscribe("fal-ai/qwen-image-edit", {
     input: {
       prompt: `8-bit pixel-art portrait, chest-up view. Keep the person's likeness and features recognizable. Use a simple solid color background. Style should be cartoonish, anime inspired, cute and tender soft. Maintain the original pose and expression.`,
